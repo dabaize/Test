c452529bd1c30123f64586e413e5cc7df8e1c884
baize97 <598692503@qq.com>
2020-05-13 16:57:55 +0800

    a

---
+++ Assets/QmToggle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class QmToggle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (GameManager.isClickDown)
+        {
+            GetComponent<Toggle>().isOn = true;
+        }
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //if (GameManager.isClickDown)
+        //{
+        // //   GetComponent<Toggle>().isOn = false;
+        //}
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (GameManager.isClickDown)
+        {
+            //transform.parent.GetComponent<ToggleGroup>().enabled = true;\
+            //GetComponent<Toggle>().isOn = false;
+        }
+        else
+        {
+            //GetComponent<Toggle>().isOn = false;
+            //transform.parent.GetComponent<ToggleGroup>().enabled = false;
+        }
+
+        GameManager.isClickDown = !GameManager.isClickDown;
+    }
+
+    public void Setbool()
+    {
+        GameManager.isClickDown = false;
+        GetComponent<Toggle>().isOn = false;
+    }
+}
---
+++ Assets/Scripts/Editortool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Editortool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [UnityEditor.MenuItem("tools/removecomponent")]
+    static void Remcomp()
+    {
+
+    }
+
+    [UnityEditor.MenuItem("tools/TestTool")]
+    static void TestTool()
+    {
+        GameObject go = Selection.activeGameObject;
+        if (go.GetComponent<MeshFilter>().sharedMesh)
+        {
+            Debug.Log("ok");
+        }
+        else
+        {
+            Debug.Log("nook");
+
+        }
+
+    }
+}
